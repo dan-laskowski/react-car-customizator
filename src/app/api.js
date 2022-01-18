8055f24454385891b17e7110944a91296485283c
dan-laskowski <daniel.laskowski94@gmail.com>
2022-01-18 14:25:40 +0100

    add function to get colors from json file

--- src/app/api.js
+++ src/app/api.js
@@ -1,5 +1,11 @@
 export async function getModels() {
-    const results = await fetch('./models.json');
-    const models = results.json();
-    return models;
+  const results = await fetch('./models.json');
+  const models = results.json();
+  return models;
+}
+
+export async function getColors() {
+  const results = await fetch('./colors.json');
+  const colors = results.json();
+  return colors;
 }
