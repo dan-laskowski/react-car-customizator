3213189451727845e2eb01d1372fa81720682843
Daniel Laskowski <13076235+dan-laskowski@users.noreply.github.com>
2026-08-09 12:19:40 +0200

    Add Husky, Prettier, and lint-staged (#7)
    * apply prettier formatting
    
    * fix eslint warnings after enabling typescript linting

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,14 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
 
-export default [
+export default tseslint.config(
   { ignores: ['dist'] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     languageOptions: {
       ecmaVersion: 2022,
       globals: globals.browser,
@@ -20,7 +23,6 @@ export default [
       'react-refresh': reactRefresh,
     },
     rules: {
-      ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
@@ -28,4 +30,5 @@ export default [
       ],
     },
   },
-];
+  prettier,
+);
